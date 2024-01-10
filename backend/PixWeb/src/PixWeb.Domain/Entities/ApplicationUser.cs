a990e1bdb088e37bec5be56d4829799114d6fcd1
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-01-10 17:47:58 -0300

    Criacao das models na camada Domain

---
+++ backend/PixWeb/src/PixWeb.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PixWeb.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ backend/PixWeb/src/PixWeb.Domain/Enums/KeyType.cs
@@ -0,0 +1,10 @@
+﻿namespace PixWeb.Domain.Enums
+{
+    public enum KeyType
+    {
+        CPF_CNPJ, 
+        Email,
+        PhoneNumber,
+        RandomKey,
+    }
+}
