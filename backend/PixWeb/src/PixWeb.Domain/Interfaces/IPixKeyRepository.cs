5d25ded77fc272894ddafff6493424e4f64b194e
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-01-11 09:49:34 -0300

    Criacao da interface IPixKeyRepository na camada Domain

---
+++ backend/PixWeb/src/PixWeb.Domain/Interfaces/IPixKeyRepository.cs
@@ -0,0 +1,15 @@
+﻿using PixWeb.Domain.Entities;
+
+namespace PixWeb.Domain.Interfaces
+{
+    public interface IPixKeyRepository
+    {
+        Task<IEnumerable<PixKey>> GetAllAsync(string userId);
+        Task<PixKey> GetByIdAsync(string userId, int id);
+        Task<PixKey> GetByKeyAsync(string userId, string key);
+        Task<PixKey> AddAsync(PixKey entity);
+        Task<PixKey> UpdateAsync(PixKey entity);
+        Task DeleteAsync(PixKey entity);
+    }
+}
+
