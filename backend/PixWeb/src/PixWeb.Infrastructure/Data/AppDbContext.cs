372b712a460eab5fb41879df0433e7eb598582ee
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-01-11 09:28:16 -0300

    Criacao do DbContext na camada Infrastructure

---
+++ backend/PixWeb/src/PixWeb.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PixWeb.Domain.Entities;
+
+namespace PixWeb.Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<PixKey> PixKeys { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().ToTable("usuarios");
+            builder.Entity<PixKey>()
+                .ToTable("chaves_pix")
+                .HasIndex(c => new { c.Key, c.UserId })
+                .IsUnique();
+        }
+    }
+}
