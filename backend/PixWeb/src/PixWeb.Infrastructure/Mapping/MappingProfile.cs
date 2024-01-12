5af538a5e8f512730eb31b3e2e3471a851740ef5
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-01-12 10:25:04 -0300

    Criacao dos mappings na camada Infrastructure

---
+++ backend/PixWeb/src/PixWeb.Infrastructure/Mapping/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using PixWeb.Application.Dtos;
+using PixWeb.Domain.Entities;
+
+namespace PixWeb.Infrastructure.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<PixKeyCreateDto, PixKey>();
+            CreateMap<PixKeyCreateDto, PixKeyDto>().ReverseMap();
+            CreateMap<PixKeyUpdateDto, PixKey>();
+            CreateMap<PixKeyUpdateDto, PixKeyDto>().ReverseMap();
+            CreateMap<PixKey, PixKeyDto>().ReverseMap();
+
+            CreateMap<UserRegistrationDto, ApplicationUser>().ReverseMap();
+            CreateMap<UserLoginDto, ApplicationUser>().ReverseMap();
+        }
+    }
+
+}
