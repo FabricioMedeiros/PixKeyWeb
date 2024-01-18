f63a44c09e03b11ca109f064a864499d21274d99
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-01-18 20:49:13 -0300

    Criacao da BaseService na camada Application para que possa ser utilizado o envio de notificacoes nos demais servicos da API.

---
+++ backend/PixWeb/src/PixWeb.Application/Services/BaseService.cs
@@ -0,0 +1,25 @@
+﻿using PixWeb.Application.Notifications;
+using System.Security.Claims;
+
+namespace PixWeb.Application.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotificator _notificator;
+        private readonly ClaimsPrincipal _currentUser;
+        protected String UserId { get; private set; }
+
+        protected BaseService(INotificator notificator, ClaimsPrincipal currentUser)
+        {
+            _notificator = notificator;
+            _currentUser = currentUser;
+
+            UserId = _currentUser.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+
+        protected void Notify(string message)
+        {
+            _notificator.AddNotification(new Notification(message));
+        }
+    }
+}
