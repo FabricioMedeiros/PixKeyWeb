d3a4f4e8bd3ff6b149e6c60af04520464d1a4d7d
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-01-18 20:25:51 -0300

    Criacao da controller PixKey na camada Presentation

---
+++ backend/PixWeb/src/PixWeb.Presentation/Controllers/PixKeyController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PixWeb.Application.Dtos;
+using PixWeb.Application.Services;
+
+namespace PixWeb.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class PixKeyController : ControllerBase
+    {
+        private readonly IPixKeyService _pixKeyService;
+
+        public PixKeyController(IPixKeyService pixKeyService)
+        {
+            _pixKeyService = pixKeyService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PixKeyDto>>> GetPixKeys()
+        {
+            var pixKeys = await _pixKeyService.GetAllAsync();
+            return Ok(pixKeys);
+        }
+
+        [HttpGet("{key}")]
+        public async Task<ActionResult<PixKeyDto>> GetPixKey(string key)
+        {
+            var pixKey = await _pixKeyService.GetByKeyAsync(key);
+
+            if (pixKey == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pixKey);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PixKeyDto>> CreatePixKey(PixKeyCreateDto pixKeyCreateDto)
+        {
+            var createdPixKey = await _pixKeyService.CreateAsync(pixKeyCreateDto);
+            return Ok(createdPixKey);
+        }
+
+
+        [HttpPut()]
+        public async Task<ActionResult<PixKeyDto>> UpdatePixKey(PixKeyUpdateDto pixKeyUpdateDto)
+        {
+            var updatedPixKey = await _pixKeyService.UpdateAsync(pixKeyUpdateDto);
+
+            if (updatedPixKey == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(updatedPixKey);
+        }
+
+        [HttpDelete("{key}")]
+        public async Task<IActionResult> DeletePixKey(string key)
+        {
+            var result = await _pixKeyService.DeleteAsync(key);
+
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+    }
+}
+
--- backend/PixWeb/src/PixWeb.Presentation/Program.cs
+++ backend/PixWeb/src/PixWeb.Presentation/Program.cs
@@ -1,11 +1,16 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PixWeb.API.Entities;
 using PixWeb.Application.Services;
 using PixWeb.Domain.Entities;
+using PixWeb.Domain.Interfaces;
 using PixWeb.Infrastructure.Data;
+using PixWeb.Infrastructure.Mapping;
+using PixWeb.Infrastructure.Repositories;
 using System.Security.Claims;
 using System.Text;
 
@@ -20,7 +25,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 //Add. Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
-    options.User.RequireUniqueEmail = true; 
+    options.User.RequireUniqueEmail = true;
     options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 -._@+";
 })
 .AddEntityFrameworkStores<AppDbContext>()
@@ -29,10 +34,19 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 //Add . Dependecy Injection
 builder.Services.AddScoped<UserManager<ApplicationUser>>();
 builder.Services.AddScoped<IPixKeyService, PixKeyService>();
+builder.Services.AddScoped<IPixKeyRepository, PixKeyRepository>();
 builder.Services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>()?.HttpContext?.User);
 
 //Add .Auto Mapper
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+var mappingConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new MappingProfile());
+});
+
+IMapper mapper = mappingConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
 
 //Add. JWT
 var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
@@ -59,7 +73,35 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "PixWeb API", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Por favor informe o token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 var app = builder.Build();
 
