421f678ae982a44694fd35941db2a189609dd0bf
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-01-24 20:00:30 -0300

    Criacao das regras de validacao utilizando o FluentValidation na camada Application

---
+++ backend/PixWeb/src/PixWeb.Application/Validators/DocmentValidator.cs
@@ -0,0 +1,36 @@
+﻿using DocumentValidator;
+using FluentValidation;
+
+namespace PixWeb.Application.Validators
+{
+    public class CpfValidator : AbstractValidator<string>
+    {
+        public CpfValidator()
+        {
+            RuleFor(cpf => cpf)
+                .Must(ValidateCpf)
+                .WithMessage("CPF inválido.");
+        }
+
+        private bool ValidateCpf(string cpf)
+        {
+            return CpfValidation.Validate(cpf);
+        }
+    }
+
+    public class CnpjValidator : AbstractValidator<string>
+    {
+        public CnpjValidator()
+        {
+            RuleFor(cnpj => cnpj)
+                .Must(ValidateCnpj)
+                .WithMessage("CNPJ inválido.");
+        }
+
+        private bool ValidateCnpj(string cnpj)
+        {
+            return CnpjValidation.Validate(cnpj);
+        }
+    }
+
+}
---
+++ backend/PixWeb/src/PixWeb.Application/Validators/PixKeyValidator.cs
@@ -0,0 +1,68 @@
+﻿using FluentValidation;
+using PixWeb.Application.Dtos;
+using PixWeb.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace PixWeb.Application.Validators
+{
+    public class PixKeyValidator : AbstractValidator<PixKeyBaseDto>
+    {
+        public PixKeyValidator()
+        {
+
+            RuleFor(x => x.Description)
+                 .NotEmpty().WithMessage("O campo Description é obrigatório.");
+
+            RuleFor(x => x.IsPersonalKey)
+               .NotNull()
+               .WithMessage("O campo IsPersonalKey é obrigatório.");
+
+            RuleFor(x => x.KeyType)
+                .NotNull()
+                .WithMessage("O campo KeyType é obrigatório.")
+                .Must(IsValidKeyType)
+                .WithMessage("O valor fornecido para KeyType não é válido.");
+
+            RuleFor(pixKey => pixKey.Key)
+                .NotEmpty().WithMessage("O campo Key é obrigatório.")
+                .Must((pixKey, key) => ValidateKeyByType(pixKey.KeyType, key))
+                .WithMessage("A chave não é válida para o tipo especificado.");
+
+            //RuleFor(x => x.Key)
+            //     .NotEmpty().WithMessage("O campo Key é obrigatório.");           
+
+            //RuleFor(pixKey => pixKey.Key)
+            //    .Must((pixKey, key) => ValidateKeyByType(pixKey.KeyType, key))
+            //    .WithMessage("A chave não é válida para o tipo especificado.");
+
+        }
+
+        private bool IsValidKeyType(KeyType keyType)
+        {
+            return Enum.IsDefined(typeof(KeyType), keyType);
+        }
+
+        private bool ValidateKeyByType(KeyType keyType, string key)
+        {
+            switch (keyType)
+            {
+                case KeyType.CPF_CNPJ:
+                    return new CpfValidator().Validate(new ValidationContext<string>(key)).IsValid ||
+                           new CnpjValidator().Validate(new ValidationContext<string>(key)).IsValid;
+                case KeyType.Email:
+                    return new EmailAddressAttribute().IsValid(key);
+                case KeyType.PhoneNumber:
+                    return ValidatePhoneNumber(key);
+                case KeyType.RandomKey:
+                    return Guid.TryParse(key, out _);
+                default:
+                    return false;
+            }
+        }
+
+        private bool ValidatePhoneNumber(string key)
+        {
+            return key.Length >= 14 && key.Length <= 15;
+        }
+    }
+}
