1ad1ddee33079dde85fa957418a0112ff34648e9
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-02-21 10:37:41 -0300

    criacao da classe responsavel por salvar e limpar os dados do usuário localmente

---
+++ frontend/PixWeb/src/app/utils/localstorage.ts
@@ -0,0 +1,50 @@
+import { jwtDecode } from "jwt-decode";
+import { JwtToken } from "../account/models/jtw.token";
+
+export class LocalStorageUtils {
+
+    public getUser() {
+        const userJson = localStorage.getItem('pixweb.user');
+        return userJson ? JSON.parse(userJson) : null;
+    }
+
+    public saveLocalUserData(response: any) {
+        if (response && response.token) {
+            this.saveTokenUser(response.token);
+    
+            try {
+                const decodedToken = jwtDecode<JwtToken>(response.token);
+            
+                this.saveUser(decodedToken.username);
+                this.saveEmailUser(decodedToken.email);
+            } catch (error) {
+                console.error('Erro ao decodificar o token:', error);
+            }
+        } else {
+            console.error('Token inválido:', response);
+        }
+    }    
+
+    public clearLocalUserData() {
+        localStorage.removeItem('pixweb.token');
+        localStorage.removeItem('pixweb.user');
+        localStorage.removeItem('pixweb.email');
+    }
+
+    public getTokenUser(): string | null {
+        return localStorage.getItem('pixweb.token');
+    }
+
+    public saveTokenUser(token: string) {
+        localStorage.setItem('pixweb.token', token);
+    }
+
+    public saveUser(user: string) {
+        localStorage.setItem('pixweb.user', JSON.stringify(user));
+    }
+
+    public saveEmailUser(email: string) {
+        localStorage.setItem('pixweb.email', JSON.stringify(email));
+    }
+
+}
