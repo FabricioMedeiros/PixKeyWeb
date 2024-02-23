4da4a68948dd8ff54f121eb27fe668dd3a4e6ad6
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-02-23 14:10:29 -0300

    Criacao do componente menu-login responsavel por controlar os menus ques serao exibidos caso o usuario esteja logado ou nao na aplicacao

--- frontend/PixWeb/src/app/services/base.service.ts
+++ frontend/PixWeb/src/app/services/base.service.ts
@@ -1,11 +1,12 @@
 import { HttpErrorResponse, HttpHeaders } from "@angular/common/http";
 import { throwError } from "rxjs";
 import { LocalStorageUtils } from "../utils/localstorage";
+import { environment } from "../../environments/environment";
 
 export abstract class BaseService {
 
     public LocalStorage = new LocalStorageUtils();
-    protected UrlServiceV1: string = 'https://localhost:7217/api/';   
+    protected UrlServiceV1: string = environment.apiUrlv1;   
 
     protected GetHeaderJson() {
         return {
