02a34e0f85a1d09a4f341daaf7cd9845ed6da45c
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-02-26 19:51:12 -0300

    Instalacao do pacote gnx-toastr

--- frontend/PixWeb/src/app/app.module.ts
+++ frontend/PixWeb/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationModule } from './navigation/navigation.module';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,9 @@ import { NavigationModule } from './navigation/navigation.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavigationModule
+    NavigationModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
