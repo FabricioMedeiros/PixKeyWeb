5e86e637abc85304535d0088228fbd85c303c6f0
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-02-27 20:28:48 -0300

    Aplicando o guarda de rota no modulo de contas

--- frontend/PixWeb/src/app/account/account-routing.module.ts
+++ frontend/PixWeb/src/app/account/account-routing.module.ts
@@ -4,17 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountAppComponent } from './account.app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { canActivate, canDeactivate } from './services/account.guard';
 
 const routes: Routes = [
     {
         path: '', component: AccountAppComponent,
         children: [
-            { path: 'register', component: RegisterComponent,},
-            { path: 'login', component: LoginComponent }
+            { path: 'register', component: RegisterComponent, canActivate: [canActivate], canDeactivate: [canDeactivate]},
+            { path: 'login', component: LoginComponent , canActivate: [canActivate]}
         ]
     }
 ];
-
 @NgModule({
     imports: [
         RouterModule.forChild(routes)
--- frontend/PixWeb/src/app/account/account.module.ts
+++ frontend/PixWeb/src/app/account/account.module.ts
@@ -29,7 +29,7 @@ import { NarikCustomValidatorsModule } from '@narik/custom-validators';
     NarikCustomValidatorsModule
   ],
   providers: [
-    AccountService    
+    AccountService  
   ]
 })
 export class AccountModule { }
