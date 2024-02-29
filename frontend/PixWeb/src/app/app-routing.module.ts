71692503b265df8c64fede4054bcdca825972a23
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-02-29 17:12:13 -0300

    Configuracao das novas rotas do modulo pixkey no modulo principal da aplicacao

--- frontend/PixWeb/src/app/app-routing.module.ts
+++ frontend/PixWeb/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './navigation/home/home.component';
+import { NotFoundComponent } from './navigation/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)}
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule)},
+  { path: 'pixkey', loadChildren: () => import('./pixkey/pixkey.module').then(m => m.PixkeyModule)},
+  { path: '**', component: NotFoundComponent },
+  { path: 'not-found', component: NotFoundComponent },
 ];
 
 @NgModule({
