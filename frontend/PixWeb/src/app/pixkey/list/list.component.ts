bdcbea9af247e0fd76da1d23b2f93c7857916311
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-03-04 20:29:02 -0300

    Implementacao do componente list que ira listas as chaves pix do usuario

--- frontend/PixWeb/src/app/pixkey/list/list.component.ts
+++ frontend/PixWeb/src/app/pixkey/list/list.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { PixKeyService } from './../services/pixkey.service';
+import { Component, OnInit } from '@angular/core';
+import { PixKey } from '../models/pixkey';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  
+  public pixKeys: PixKey[] = [];;
+  errorMessage: string = '';
+
+  constructor(private pixKeyService: PixKeyService) { }
+
+  ngOnInit(): void {
+    this.pixKeyService.getAll().subscribe({
+      next: pixKeys => {
+        this.pixKeys = pixKeys;
+      },
+      error: error => {
+        this.errorMessage = error;
+      },
+    });    
+  }
+
+  getKeyTypeDescription(keyType: number): string {
+    switch (keyType) {
+      case 0:
+        return 'CPF/CNPJ';
+      case 1:
+        return 'E-mail';
+      case 2:
+          return 'Telefone';  
+      case 3:
+          return 'Aleatória';      
+      default:
+        return 'Desconhecido';
+    }
+  }
 
 }
