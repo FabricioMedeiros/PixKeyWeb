323749858e3cf766957d4349765079f0760a7de2
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-03-12 11:50:58 -0300

    Implementacao dos metodos de integracao do endpont pixkey

--- frontend/PixWeb/src/app/pixkey/services/pixkey.service.ts
+++ frontend/PixWeb/src/app/pixkey/services/pixkey.service.ts
@@ -2,18 +2,56 @@ import { Injectable } from "@angular/core";
 import { BaseService } from "../../services/base.service";
 import { HttpClient } from "@angular/common/http";
 import { PixKey } from "../models/pixkey";
-import { Observable, catchError } from "rxjs";
+import { Observable, catchError, map } from "rxjs";
 
 @Injectable()
 export class PixKeyService extends BaseService {
 
     constructor(private http: HttpClient) { super(); }
 
-    getAll(): Observable<PixKey[]> {
+    getAllPixKeys(): Observable<PixKey[]> {
         const headers = this.GetAuthHeaderJson();
 
         return this.http
             .get<PixKey[]>(this.UrlServiceV1 + "pixkey", headers)
+            .pipe(catchError(super.serviceError));
+    }
+
+    getPixKeyById(id: string): Observable<PixKey> {
+        const headers = this.GetAuthHeaderJson();
+
+        return this.http
+            .get<PixKey>(`${this.UrlServiceV1}pixkey/${id}`, headers)
+            .pipe(catchError(super.serviceError));
+    }
+
+    registerPixKey(pixKey: PixKey): Observable<PixKey> {
+        const headers = this.GetAuthHeaderJson();
+
+        let response = this.http
+            .post(this.UrlServiceV1 + 'PixKey', pixKey, headers)
+            .pipe(
+                map(this.extractData),
+                catchError(this.serviceError));
+
+        return response;
+    }
+
+    updatePixKey(pixKey: PixKey): Observable<PixKey> {
+        let response = this.http
+            .put(this.UrlServiceV1 + 'pixkey', pixKey, this.GetAuthHeaderJson())
+            .pipe(
+                map(this.extractData),
+                catchError(this.serviceError));
+
+        return response;
+    }
+
+    deletePixKey(id: string): Observable<any> {
+        const headers = this.GetAuthHeaderJson();
+
+        return this.http
+            .delete(`${this.UrlServiceV1}pixkey/${id}`, headers)
             .pipe(catchError(super.serviceError));
     }
 
