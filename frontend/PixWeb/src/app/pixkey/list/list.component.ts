cd724416c45cef4e8c239fba365563c9347247fd
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-03-21 11:48:12 -0300

    Implementacao do Modal e metodos para exclusao do registro no component list do modulo PixKey

--- frontend/PixWeb/src/app/pixkey/list/list.component.ts
+++ frontend/PixWeb/src/app/pixkey/list/list.component.ts
@@ -1,7 +1,12 @@
-import { PixKeyService } from './../services/pixkey.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { PixKey } from '../models/pixkey';
 import { Router } from '@angular/router';
+
+import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+
+import { PixKeyService } from './../services/pixkey.service';
+
 
 @Component({
   selector: 'app-list',
@@ -10,10 +15,19 @@ import { Router } from '@angular/router';
 })
 export class ListComponent implements OnInit {
   
-  public pixKeys: PixKey[] = [];;
+  public pixKeys: PixKey[] = [];
   errorMessage: string = '';
+  selectedPixKey!: PixKey;
+  
+  bsModalRef!: BsModalRef;
+  @ViewChild('deleteModal') deleteModal!: TemplateRef<any>;  
 
-  constructor(private pixKeyService: PixKeyService, private router: Router) { }
+  constructor(
+    private pixKeyService: PixKeyService, 
+    private router: Router,
+    private modalService: BsModalService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
     this.pixKeyService.getAllPixKeys().subscribe({
@@ -45,4 +59,37 @@ export class ListComponent implements OnInit {
     this.router.navigate(['/pixkey/edit', pixKey.id]);
   }
 
+  openDeleteModal(template: TemplateRef<any>, pixKey : PixKey) {
+    this.selectedPixKey = pixKey;
+    this.bsModalRef = this.modalService.show(template, this.selectedPixKey);
+  }
+
+  confirmDelete(pixKey: PixKey): void {  
+    this.pixKeyService.deletePixKey(pixKey.id).subscribe({
+      next: success => {
+        this.processSuccess(success, pixKey);
+      },
+      error: error => {
+        this.processFail(error);
+      }
+    });
+  }
+
+  processSuccess(response: any, pixKey: PixKey) {
+    this.pixKeys = this.pixKeys.filter(item => item.id !== pixKey.id);
+    this.bsModalRef.hide();
+    
+    let toast = this.toastr.success('Registro Excluído com Sucesso!', 'Atenção!');
+
+    if (toast) {
+      toast.onHidden.subscribe(() => {
+        this.pixKeys = this.pixKeys.filter(item => item.id !== pixKey.id);
+        this.bsModalRef.hide();
+      });
+    }
+  }
+
+  processFail(fail: any) {
+    this.toastr.error('Ocorreu um erro.', 'Atenção');
+  }
 }
