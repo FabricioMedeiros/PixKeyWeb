59829798d28c7636f9d9a232e2d6412deb7bee9c
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-03-25 20:39:24 -0300

    Configuracao do spinner no modulo principal

--- frontend/PixWeb/src/app/app.module.ts
+++ frontend/PixWeb/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationModule } from './navigation/navigation.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { BsModalService, ModalModule } from 'ngx-bootstrap/modal';
 
+import { NavigationModule } from './navigation/navigation.module';
 import { LocalStorageUtils } from './utils/localstorage';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @NgModule({
   declarations: [
@@ -20,10 +20,12 @@ import { LocalStorageUtils } from './utils/localstorage';
     AppRoutingModule,
     NavigationModule,
     BrowserAnimationsModule,
+    NgxSpinnerModule,
     ToastrModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
   ],
   providers: [LocalStorageUtils, BsModalService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
