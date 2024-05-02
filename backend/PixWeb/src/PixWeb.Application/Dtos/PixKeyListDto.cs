1c9faba17292b0f109f8686261ef1716c5c81dec
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-05-02 20:58:22 -0300

    criacao da nova dto para realizar a paginacao com a lista das chaves pix de acordo com a pagina e a quantidade registros requisitados pelo frontend.

---
+++ backend/PixWeb/src/PixWeb.Application/Dtos/PixKeyListDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PixWeb.Application.Dtos
+{
+    public class PixKeyListDto
+    {
+        public int TotalRecords { get; set; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+
+        public IEnumerable<PixKeyDto>? PixKeys { get; set; }
+    }
+}
