674df5f50e8d7d13c1e300c76b86afef79169cb2
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-05-18 16:05:42 -0300

    Criacao da classe BaseRepository para permitir lancar notificacoes nos repositorios

---
+++ backend/PixWeb/src/PixWeb.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using PixWeb.Application.Notifications;
+
+namespace PixWeb.Infrastructure.Repositories
+{
+    public abstract class BaseRepository
+    {
+        private readonly INotificator _notificator;
+
+        protected BaseRepository(INotificator notificator)
+        {
+            _notificator = notificator;
+        }
+
+        protected void Notify(string message)
+        {
+            _notificator.AddNotification(new Notification(message));
+        }
+    }
+}
+
