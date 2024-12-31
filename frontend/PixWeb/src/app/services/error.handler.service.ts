e5ebe7eb01f694d8c600199e8c9258f820a658ad
Fabrício Medeiros <fabriciomedeiros@hotmail.com.br>
2024-12-31 17:40:23 -0300

    Alteracao para exibir um toastr com a mensagem de sessao encerrada quando a validade do token expirar

--- frontend/PixWeb/src/app/services/error.handler.service.ts
+++ frontend/PixWeb/src/app/services/error.handler.service.ts
@@ -3,15 +3,18 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse
 
 import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 import { LocalStorageUtils } from '../utils/localstorage';
-import { Router } from '@angular/router';
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(private router: Router) { }
+    constructor(private router: Router,
+        private toastr: ToastrService
+    ) { }
 
     localStorageUtil = new LocalStorageUtils();
 
@@ -23,7 +26,16 @@ export class ErrorInterceptor implements HttpInterceptor {
                     this.router.navigate(['/service-unavailable']);
                 } else if (error.status === 401) {
                     this.localStorageUtil.clearLocalUserData();
-                    this.router.navigate(['/account/login'], {queryParams: {returnUrl: this.router.url}});
+
+                    this.toastr.clear();
+                    this.toastr.warning('Sessão encerrada. Faça login novamente.', 'Atenção');
+                    this.router.navigate(['/account/login'], { queryParams: { returnUrl: this.router.url } });
+
+                    return throwError(() => new HttpErrorResponse({
+                        error: { message: 'Sessão encerrada.' },
+                        status: 401,
+                        statusText: 'Unauthorized'
+                    }));
                 }
 
                 throw error;
